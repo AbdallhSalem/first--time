d26b53afccd0b70beff628d374932fcf3de3314f
Your Name <you@example.com>
2022-11-26 18:21:05 +0200

    added code files

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM openjdk
+
+
+WORKDIR /application
+
+
+COPY abdallah.java .
+
+
+
+RUN  javac abdallah.java
+
+
+CMD java abdallah
---
+++ abdo.java
@@ -0,0 +1,12 @@
+public class abdallah{
+	public static void main(String[]args) {
+		
+		
+		System.out.println("abdallah");
+	}
+	
+	
+	
+	
+	
+}
